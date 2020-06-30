f80ee27b48b82904ad20440a04e70e7e07b7efa3
drogel <diegorogel3d@hotmail.com>
2020-06-30 12:06:50 +0200

    updates to colored v0.9 preview

--- colored/flutter_service_worker.js
+++ colored/flutter_service_worker.js
@@ -1,15 +1,15 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "b82fe9391a736dfc1c025489664095a4",
-"/": "b82fe9391a736dfc1c025489664095a4",
-"main.dart.js": "74b700814326116172a5683966096014",
+  "index.html": "fe2b9f8670e71fe0c2b6cb24ab17ce16",
+"/": "fe2b9f8670e71fe0c2b6cb24ab17ce16",
+"main.dart.js": "6a12a31d3ed617c385e09c83ee8860d6",
 "favicon.png": "c4aec8a33148b14ac2de4d7cdfb4dc55",
 "icons/Icon-192.png": "2c71de455f898b3f916a2cfe25fac773",
 "icons/Icon-512.png": "f470c5afc2b4b1911c3522851dd3c2b7",
 "manifest.json": "f8cf68546a656247f71c4fa9c0466662",
 "assets/LICENSE": "39b31e655d66cf5fef38e28315d2ba1a",
-"assets/AssetManifest.json": "0cda41400d2ab215c4b76b50a23108fe",
+"assets/AssetManifest.json": "16774fcb6bb77c726c83fc3f0585d130",
 "assets/FontManifest.json": "a672f412921bb996d6e450988a6d1c45",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/lib/resources/mock_data/sample_color_names.json": "e242dd7401bef244692ed8cef3b636d7",
@@ -26,6 +26,7 @@ const RESOURCES = {
 "assets/lib/resources/fonts/montserrat/Montserrat-Regular.ttf": "ee6539921d713482b8ccd4d0d23961bb",
 "assets/lib/resources/fonts/montserrat/Montserrat-Italic.ttf": "a7063e0c0f0cb546ad45e9e24b27bd3b",
 "assets/lib/resources/data/color_names.json": "cb8e115ebbc1ec1b1ec4b70ccde1b151",
+"assets/lib/resources/data/color_suggestions.json": "a0a0dfbee449db9d73ec07de4630cf98",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
